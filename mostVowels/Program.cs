a5cde7c152035709c98a0a6af2820958735bc9aa
Louy Alsadi <louyalsadi@Louys-MacBook-Pro.local>
2020-06-20 01:04:31 -0500

    mostVowels method

---
+++ mostVowels/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace mostVowels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = " hello world this is Leonardo";
+            string v = MostVowels(myString);
+            Console.WriteLine(v);
+        }
+        static string MostVowels(string text)
+        {
+            string maxWord = "";
+            int maxVoels = 0;
+            string Vowels = "aeiouAEIUOU";
+            string[] wordsArr = text.Split(" ");
+            foreach (string word in wordsArr)
+            {
+                //Console.WriteLine(word);
+                int counter = 0;
+                foreach (char letter in word)
+                {
+                    //Console.WriteLine(letter);
+                    if (Vowels.IndexOf(letter) > -1)
+                    {
+                        counter += 1;
+                        if (counter > maxVoels)
+                        {
+                            maxVoels = counter;
+                            maxWord = word;
+
+                        }
+                    }
+                }
+
+            }
+            return ($"{maxWord} has {maxVoels} Vowles");
+        }
+    }
+}
